2e9383a0b946faa07193a223d6670a00b69b6973
chenxinzhou <chenxinzhou@antiy.cn>
2017-09-06 18:46:57 +0800

    修改列子元素个数

--- src/modules/Analyse_AttackChain/components/TableConfig.js
+++ src/modules/Analyse_AttackChain/components/TableConfig.js
@@ -5,14 +5,18 @@ import React from 'react';
 import tableColumnsGenerator from '../../../utils/tableColumnsGenerator';
 import JoTag from '../../../components/JoTag';
 import constConfig,{stageRowDataIndexes,rowDataIndexes} from '../ConstConfig';
-import {Icon,Switch,Card,Timeline} from 'antd';
+import {Icon,Switch,Card,Timeline,Row,Col} from 'antd';
 import classnames from 'classnames';
 import * as tools from '../../../utils/tools';
 import TimeLabel from '../../../components/TImesLabel';
+import styles from './TableConfig.css';
 
 const {tableTextConfig}=constConfig;
 
 const stageRowRenderer=(value)=>{
+  if(!value){
+    return;
+  }
   return (
     <div>
       {value.map((i,index)=>{
@@ -28,9 +32,7 @@ const attackTimesRenderer=value=>{
 
 export const getColumns=({queryFilters,getCheckboxOnChange})=>{
 
-  const renderer={
-          attackTimes:attackTimesRenderer
-    },
+  const renderer={attackTimes:attackTimesRenderer},
         titleRenderer={};
 
 
@@ -65,36 +67,54 @@ export const getExpandedRowRender=({isDark})=>{
     const classes=classnames({
       ["expanded-row-dark"]:isDark,
       ["expanded-row-card-item"]:true,
+      [styles["flex-item"]]:true,
     });
 
     const cardKeys=Object.keys(records).filter(i=>stageRowDataIndexes.indexOf(i)!==-1)
 
+
+    const rows=[];
+
+    const ROW_ITEM_COUNT=2;
+
+    cardKeys.forEach((k,index)=>{
+      const ROW_INDEX=Math.round((index+1)/ROW_ITEM_COUNT)-1;
+
+      rows[ROW_INDEX]=rows[ROW_INDEX]||[];
+      rows[ROW_INDEX].push(<Card title={tools.getKeyText(k,title)}
+                                 key={`${k}-card`}
+                                 className={classes}>
+        <Timeline>
+          {records[k].map((i,index)=>{
+            return (
+              <Timeline.Item key={`${index}-timeline-item`}>
+                <div>
+                  <JoTag color="#108ee9">{i.title}</JoTag>
+                  <br/><br/>
+                  {expandedRow.description+":"}{i.details}
+                </div>
+              </Timeline.Item>
+            )
+          })}
+        </Timeline>
+      </Card>);
+
+    })
+
+
     return (
       <div>
-        {cardKeys.map(k=>{
+        {rows.map((r,index)=>{
           return (
-            <Card title={tools.getKeyText(k,title)}
-                  key={`${k}-card`}
-                  className={classes}>
-              <Timeline>
-                {records[k].map((i,index)=>{
-                  return (
-                    <Timeline.Item key={`${index}-timeline-item`}>
-                      <div>
-                        <JoTag color="#108ee9">{i.title}</JoTag>
-                        <br/><br/>
-                        {expandedRow.description+":"}{i.details}
-                      </div>
-                    </Timeline.Item>
-                  )
-                })}
-              </Timeline>
-            </Card>
+            <div key={`${index}-row`}
+                 className={styles["flex"]}>
+              {r}
+            </div>
           )
         })}
       </div>
+
     )
 
   };
 };
-
