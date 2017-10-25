66d7d813dbfb9028b1cdbca76df1918243d6d04a
chenxinzhou <chenxinzhou@antiy.cn>
2017-10-25 11:56:56 +0800

    modify: strategy module rule -  ipport reg change

--- src/modules/SysConfig_Strategy_Rule/components/RuleForm/ruleItemCheckConfig.js
+++ src/modules/SysConfig_Strategy_Rule/components/RuleForm/ruleItemCheckConfig.js
@@ -22,59 +22,63 @@ import {
 
 import * as tools from '../../../../utils/tools'
 
-const ipReg=tools.ipReg
-const emailReg=/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/
-const numberReg=/^[0-9]*$/;
-const checkerDecorator=fn=>(args)=>{
-  try{
+const ipReg = tools.ipReg
+const emailReg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/
+const numberReg = /^[0-9]*$/;
+const checkerDecorator = fn => (args) => {
+  try {
     fn(args)
-  }catch(e){
+  } catch (e) {
     // console.warn(e);
   }
 }
 
-const isNumberStr=(str)=>numberReg.test(str);
+const isNumberStr = (str) => numberReg.test(str);
 
-const isIpPort=(str="")=>{
-  const items=str.split(":");
-  if(items.length>2||items.length===0){
+const isIpPort = (str = "") => {
+  const items = str.split(":");
+  if (items.length > 2 || items.length === 0) {
     return false;
   }
-  if(!ipReg.test(items[0])){
-    return false;
+
+  let isIp = ipReg.test(items[0])
+
+  if (isIp && items.length === 1) {
+    return true;
   }
-  let port=items[1];
-  if(isNumberStr(port)&&parseInt(port)>=0&&parseInt(port)<=65535){
+  let port = items[1],
+    isPort = isNumberStr(port) && parseInt(port) >= 0 && parseInt(port) <= 65535
+  if (isPort && isIp) {
     return true;
   }
   return false;
 }
 
-const ipPortTypeChecker=checkerDecorator(({dataIndex,props,setCheckStatus,checkerStatus})=>{
-  let souceIpPort=props.form.getFieldValue(SOURCE_IP_PORT),
-    targetIpPort=props.form.getFieldValue(TARGET_IP_PORT);
+const ipPortTypeChecker = checkerDecorator(({ dataIndex, props, setCheckStatus, checkerStatus }) => {
+  let souceIpPort = props.form.getFieldValue(SOURCE_IP_PORT),
+    targetIpPort = props.form.getFieldValue(TARGET_IP_PORT);
 
-  souceIpPort=souceIpPort||"";
-  targetIpPort=targetIpPort||"";
+  souceIpPort = souceIpPort || "";
+  targetIpPort = targetIpPort || "";
 
-  let isSourceEmpty=souceIpPort.trim().length===0,
-      isTargetEmpty=targetIpPort.trim().length===0;
+  let isSourceEmpty = souceIpPort.trim().length === 0,
+    isTargetEmpty = targetIpPort.trim().length === 0;
 
-  if(isSourceEmpty&&isTargetEmpty){
-    setCheckStatus(TARGET_IP_PORT,"error","至少填写一项")
-    setCheckStatus(SOURCE_IP_PORT,"error","至少填写一项")
+  if (isSourceEmpty && isTargetEmpty) {
+    setCheckStatus(TARGET_IP_PORT, "error", "至少填写一项")
+    setCheckStatus(SOURCE_IP_PORT, "error", "至少填写一项")
     return;
-  }else {
-    setCheckStatus(TARGET_IP_PORT,"","")
-    setCheckStatus(SOURCE_IP_PORT,"","")
+  } else {
+    setCheckStatus(TARGET_IP_PORT, "", "")
+    setCheckStatus(SOURCE_IP_PORT, "", "")
   }
 
-  if((!isIpPort(souceIpPort))&&!isSourceEmpty){
-    setCheckStatus(SOURCE_IP_PORT,"error","请输入正确的IP:PORT")
+  if ((!isIpPort(souceIpPort)) && !isSourceEmpty) {
+    setCheckStatus(SOURCE_IP_PORT, "error", "请输入正确的IP:PORT")
   }
 
-  if((!isIpPort(targetIpPort))&&!isTargetEmpty){
-    setCheckStatus(TARGET_IP_PORT,"error","请输入正确的IP:PORT")
+  if ((!isIpPort(targetIpPort)) && !isTargetEmpty) {
+    setCheckStatus(TARGET_IP_PORT, "error", "请输入正确的IP:PORT")
   }
 
 
@@ -83,64 +87,64 @@ const ipPortTypeChecker=checkerDecorator(({dataIndex,props,setCheckStatus,checke
 
 export default {
 
-  [HTTP]:checkerDecorator(({props,setCheckStatus})=>{
-    let urlValue=props.form.getFieldValue(URL);
-        urlValue=urlValue||"";
+  [HTTP]: checkerDecorator(({ props, setCheckStatus }) => {
+    let urlValue = props.form.getFieldValue(URL);
+    urlValue = urlValue || "";
 
-    urlValue.trim().length===0
+    urlValue.trim().length === 0
       ?
-      setCheckStatus(URL,"error","不能为空")
+      setCheckStatus(URL, "error", "不能为空")
       :
-      setCheckStatus(URL,"success","");
+      setCheckStatus(URL, "success", "");
   }),
 
-  [DNS]:checkerDecorator(({props,setCheckStatus})=>{
-    let urlValue=props.form.getFieldValue(URL);
-    urlValue.trim().length===0
+  [DNS]: checkerDecorator(({ props, setCheckStatus }) => {
+    let urlValue = props.form.getFieldValue(URL);
+    urlValue.trim().length === 0
       ?
-      setCheckStatus(URL,"error","不能为空")
+      setCheckStatus(URL, "error", "不能为空")
       :
-      setCheckStatus(URL,"success","");
+      setCheckStatus(URL, "success", "");
   }),
 
-  [TCP]:ipPortTypeChecker,
+  [TCP]: ipPortTypeChecker,
 
-  [IP]:ipPortTypeChecker,
+  [IP]: ipPortTypeChecker,
 
-  [SSH]:ipPortTypeChecker,
+  [SSH]: ipPortTypeChecker,
 
-  [FTP]:ipPortTypeChecker,
+  [FTP]: ipPortTypeChecker,
 
-  [SMTP_POP3]:checkerDecorator(({props,setCheckStatus,dataIndex})=>{
-    let receiver=props.form.getFieldValue(REVEICER),
-        sender=props.form.getFieldValue(SENDER),
-        theme=props.form.getFieldValue(THEME);
+  [SMTP_POP3]: checkerDecorator(({ props, setCheckStatus, dataIndex }) => {
+    let receiver = props.form.getFieldValue(REVEICER),
+      sender = props.form.getFieldValue(SENDER),
+      theme = props.form.getFieldValue(THEME);
 
-    receiver=receiver||"";
-    sender=sender||"";
-    theme=theme||"";
+    receiver = receiver || "";
+    sender = sender || "";
+    theme = theme || "";
 
-    let isReceiverEmpty=receiver.trim().length===0,
-        isSenderEmpty=sender.trim().length===0,
-        isThemeEmpty=theme.trim().length===0;
+    let isReceiverEmpty = receiver.trim().length === 0,
+      isSenderEmpty = sender.trim().length === 0,
+      isThemeEmpty = theme.trim().length === 0;
 
-    if(isReceiverEmpty&&isThemeEmpty&&isSenderEmpty){
-      setCheckStatus(REVEICER,"error","至少填写一项")
-      setCheckStatus(SENDER,"error","至少填写一项")
-      setCheckStatus(THEME,"error","至少填写一项")
-    }else {
-      setCheckStatus(REVEICER,"","")
-      setCheckStatus(SENDER,"","")
-      setCheckStatus(THEME,"","")
+    if (isReceiverEmpty && isThemeEmpty && isSenderEmpty) {
+      setCheckStatus(REVEICER, "error", "至少填写一项")
+      setCheckStatus(SENDER, "error", "至少填写一项")
+      setCheckStatus(THEME, "error", "至少填写一项")
+    } else {
+      setCheckStatus(REVEICER, "", "")
+      setCheckStatus(SENDER, "", "")
+      setCheckStatus(THEME, "", "")
     }
 
 
-    if((!emailReg.test(receiver))&&!isReceiverEmpty){
-      setCheckStatus(REVEICER,"error","请输入正确的邮箱")
+    if ((!emailReg.test(receiver)) && !isReceiverEmpty) {
+      setCheckStatus(REVEICER, "error", "请输入正确的邮箱")
     }
 
-    if((!emailReg.test(sender))&&!isSenderEmpty){
-      setCheckStatus(SENDER,"error","请输入正确的邮箱")
+    if ((!emailReg.test(sender)) && !isSenderEmpty) {
+      setCheckStatus(SENDER, "error", "请输入正确的邮箱")
     }
 
 
