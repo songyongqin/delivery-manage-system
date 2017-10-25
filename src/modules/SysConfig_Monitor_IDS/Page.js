33f1aac8e1ec1c1ace5c3a588ad85c79e0e6aaeb
chenxinzhou <chenxinzhou@antiy.cn>
2017-10-25 16:05:04 +0800

    modify :sys-config monitor log  ids row dataindex

--- src/modules/SysConfig_Monitor_IDS/Page.js
+++ src/modules/SysConfig_Monitor_IDS/Page.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {
   NAMESPACE
 } from './ConstConfig.js'
-import { getCommonColumns } from '../SysConfig_Monitor/components/TableConfig'
+import { getNodeColumns } from '../SysConfig_Monitor/components/TableConfig'
 import monitorContainerGenerator from '../../Generators/MonitorContainerGenerator'
 
 export default monitorContainerGenerator({
   namespace: NAMESPACE,
   title: "流量监测系统",
-  getColumns: getCommonColumns
+  getColumns: getNodeColumns
 })
 
