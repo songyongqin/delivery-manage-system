844feb237a89ca7787a969f7b5855749def53468
chenxinzhou <chenxinzhou@antiy.cn>
2017-10-26 12:00:33 +0800

    modify: sys-config network put adapter api should have adapter name

--- src/modules/SysConfig_Network/Page.js
+++ src/modules/SysConfig_Network/Page.js
@@ -7,8 +7,8 @@ import {
   Badge,
   message as Message,
 } from 'antd';
-import {WithAnimateRender,WithBreadcrumb} from '../../components/HOSComponents/index'
-import {createMapDispatchWithPromise} from '../../utils/dvaExtraDispatch'
+import { WithAnimateRender, WithBreadcrumb } from '../../components/HOSComponents/index'
+import { createMapDispatchWithPromise } from '../../utils/dvaExtraDispatch'
 import Card from '../../domainComponents/Card';
 import {
   NAMESPACE,
@@ -23,125 +23,126 @@ import {
   STATUS_CONNECT,
   DNS_DATAINDEX
 } from './ConstConfig'
-import {connect} from 'dva';
+import { connect } from 'dva';
 import NetworkForm from './components/NetworkForm';
 import DNSForm from './components/DNSForm';
 import JoSpin from '../../components/JoSpin';
 import classnames from 'classnames';
 
 /*********************************************************/
-const DNSContent=({loading,defaultValue,onSubmit,isDark})=>(
-  <Card title={DNS_CONFIG_TITLE} style={{margin:"15px 0"}}>
+const DNSContent = ({ loading, defaultValue, onSubmit, isDark }) => (
+  <Card title={DNS_CONFIG_TITLE} style={{ margin: "15px 0" }}>
     <DNSForm isDark={isDark}
-             loading={loading}
-             defaultValue={defaultValue}
-             onSubmit={onSubmit}/>
+      loading={loading}
+      defaultValue={defaultValue}
+      onSubmit={onSubmit} />
   </Card>
 )
 /*********************************************************/
-const AdapterContent=({data=[],titleStyle,isDark,loading,getAdapterPutHandle})=>(
+const AdapterContent = ({ data = [], titleStyle, isDark, loading, getAdapterPutHandle }) => (
   <Card title={DNS_NETWORK_TITLE}>
-    {data.map((i,index)=>(
+    {data.map((i, index) => (
       <div key={`${index}-adapter`}
-           style={{marginBottom:"20px"}}>
+        style={{ marginBottom: "20px" }}>
         <h3 style={titleStyle}>
-          {i[ADAPTER_STATUS_DATAINDEX]===STATUS_CONNECT
+          {i[ADAPTER_STATUS_DATAINDEX] === STATUS_CONNECT
             ?
-            <Badge status="success"/>
+            <Badge status="success" />
             :
-            <Badge status="error"/>}
+            <Badge status="error" />}
           {i[ADAPTER_NAME_DATAINDEX]}
           {"("}
           {adapterStatusTextConfig[i[ADAPTER_STATUS_DATAINDEX]]}
           {")"}
         </h3>
         <NetworkForm isDark={isDark}
-                     loading={loading}
-                     defaultValue={i}
-                     onSubmit={getAdapterPutHandle(i[ADAPTER_MAC_DATAINDEX])}/>
+          loading={loading}
+          defaultValue={i}
+          onSubmit={getAdapterPutHandle(i[ADAPTER_MAC_DATAINDEX], i[ADAPTER_NAME_DATAINDEX])} />
       </div>
     ))}
   </Card>
 )
 /*********************************************************/
-const mapStateToProps=state=>({
-  [NAMESPACE]:state[NAMESPACE],
-  putLoading:state.loading.effects[`${NAMESPACE}/put`],
-  getLoading:state.loading.effects[`${NAMESPACE}/query`],
-  commonLayout:state.layout.commonLayout
+const mapStateToProps = state => ({
+  [NAMESPACE]: state[NAMESPACE],
+  putLoading: state.loading.effects[`${NAMESPACE}/put`],
+  getLoading: state.loading.effects[`${NAMESPACE}/query`],
+  commonLayout: state.layout.commonLayout
 })
 /*********************************************************/
-const mapDispatchToProps=dispatch=>({
-  get:()=>dispatch({
-    type:`${NAMESPACE}/query`
+const mapDispatchToProps = dispatch => ({
+  get: () => dispatch({
+    type: `${NAMESPACE}/query`
   }),
-  put:payload=>dispatch({
-    type:`${NAMESPACE}/put`,
+  put: payload => dispatch({
+    type: `${NAMESPACE}/put`,
     payload,
   })
 })
 /*********************************************************/
-const ADAPTER="adapter";
+const ADAPTER = "adapter";
 /*********************************************************/
-@connect(mapStateToProps,createMapDispatchWithPromise(mapDispatchToProps))
+@connect(mapStateToProps, createMapDispatchWithPromise(mapDispatchToProps))
 @WithBreadcrumb
 @WithAnimateRender
-class Page extends React.Component{
+class Page extends React.Component {
   /*********************************************************/
   constructor(props) {
     super(props);
   }
   /*********************************************************/
-  componentDidMount=()=>{
+  componentDidMount = () => {
     this.props.get();
   }
   /*********************************************************/
-  getBreadcrumb=()=>{
-    return <div key="breadcrumb-panel" style={{margin:"15px 0"}}>
+  getBreadcrumb = () => {
+    return <div key="breadcrumb-panel" style={{ margin: "15px 0" }}>
       {this.props.getBreadcrumb(this.props.routes)}
     </div>
   }
   /*********************************************************/
-  dnsPutHandle=payload=>this.props.put({
-    [DNS_DATAINDEX]:payload
+  dnsPutHandle = payload => this.props.put({
+    [DNS_DATAINDEX]: payload
   })
-    .then(Message.success.call(null,"保存成功"))
+    .then(Message.success.call(null, "保存成功"))
     .then(this.props.get.call(this))
   /*********************************************************/
-  getAdapterPutHandle=mac=>payload=>this.props.put({
-    [ADAPTER]:{
-      [ADAPTER_MAC_DATAINDEX]:mac,
-      ...(payload||{})
+  getAdapterPutHandle = (mac, name) => payload => this.props.put({
+    [ADAPTER]: {
+      [ADAPTER_MAC_DATAINDEX]: mac,
+      [ADAPTER_NAME_DATAINDEX]: name,
+      ...(payload || {})
     }
   })
-    .then(Message.success.call(null,"保存成功"))
+    .then(Message.success.call(null, "保存成功"))
     .then(this.props.get.call(this))
   /*********************************************************/
-  getContentPanel=()=>{
-    const {queryResults}=this.props[NAMESPACE],
-          {putLoading,getLoading,commonLayout}=this.props,
-          isDark=commonLayout.darkTheme;
+  getContentPanel = () => {
+    const { queryResults } = this.props[NAMESPACE],
+      { putLoading, getLoading, commonLayout } = this.props,
+      isDark = commonLayout.darkTheme;
 
-    let titleStyle={fontSize:"16px",marginBottom:"10px"};
-    titleStyle=isDark?{...titleStyle,color:"white"}:titleStyle;
+    let titleStyle = { fontSize: "16px", marginBottom: "10px" };
+    titleStyle = isDark ? { ...titleStyle, color: "white" } : titleStyle;
 
     return (
-      <div key="content-panel" style={{paddingBottom:"150px"}}>
-        <JoSpin spinning={putLoading||getLoading}>
+      <div key="content-panel" style={{ paddingBottom: "150px" }}>
+        <JoSpin spinning={putLoading || getLoading}>
           <DNSContent loading={putLoading}
-                      isDark={isDark}
-                      onSubmit={this.dnsPutHandle}
-                      defaultValue={{[DNS_DATAINDEX]:queryResults[DNS_DATAINDEX]}}/>
+            isDark={isDark}
+            onSubmit={this.dnsPutHandle}
+            defaultValue={{ [DNS_DATAINDEX]: queryResults[DNS_DATAINDEX] }} />
           <AdapterContent data={queryResults[ADAPTER_LIST_DATAINDEX]}
-                          isDark={isDark}
-                          loading={putLoading}
-                          titleStyle={titleStyle}
-                          getAdapterPutHandle={this.getAdapterPutHandle}/>
+            isDark={isDark}
+            loading={putLoading}
+            titleStyle={titleStyle}
+            getAdapterPutHandle={this.getAdapterPutHandle} />
         </JoSpin>
       </div>
     )
   }
-  render=()=>{
+  render = () => {
 
 
     return (
