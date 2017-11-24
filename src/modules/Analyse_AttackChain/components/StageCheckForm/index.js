bac94b3bf9f2e624ee9708c2e8c91575dad162e3
chenxinzhou <chenxinzhou@antiy.cn>
2017-11-24 18:46:54 +0800

    modify/attack-chain filter

--- src/modules/Analyse_AttackChain/components/StageCheckForm/index.js
+++ src/modules/Analyse_AttackChain/components/StageCheckForm/index.js
@@ -14,7 +14,7 @@ class QueryForm extends React.Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    const {onSubmit,form}=this.props;
+    const { onSubmit, form } = this.props;
     form.validateFieldsAndScroll((err, values) => {
       if (this.props.loading) {
         return;
@@ -24,41 +24,56 @@ class QueryForm extends React.Component {
         return;
       }
       onSubmit && onSubmit(values);
-      
+
     });
   };
-
+  selectAll = e => {
+    this.props.form.setFieldsValue({ [ATTACK_STAGE_DATA_INDEX]: e.target.checked ? stageRowDataIndexes : [] })
+  }
   render() {
 
-    const { getFieldDecorator } = this.props.form;
-    const { defaultValue = {}, loading = false, textConfig = {}, style = {},isDark} = this.props;
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    const { defaultValue = {}, loading = false, textConfig = {}, style = {}, isDark } = this.props;
     const { value = "" } = defaultValue;
 
-    const lblClasses=classnames({
-      ["lbl-dark"]:isDark,
+    const lblClasses = classnames({
+      ["lbl-dark"]: isDark,
     })
-    
+
+    const values = getFieldValue(ATTACK_STAGE_DATA_INDEX) || defaultValue[ATTACK_STAGE_DATA_INDEX] || [];
+
     return (
-      <Form style={{ overflow:"hidden" }} layout="inline">
+      <Form style={{ overflow: "hidden" }} layout="inline">
         <FormItem
-        label={<span className={lblClasses}>攻击阶段筛选</span>}
-        style={{float:"left"}}
-        required={false}>
+          label={<span className={lblClasses}>攻击阶段筛选</span>}
+          style={{ float: "left" }}>
+          <Checkbox
+            onChange={this.selectAll}
+            checked={values.length === stageRowDataIndexes.length}
+            indeterminate={values.length > 0 && values.length < stageRowDataIndexes.length}>
+            <span className={lblClasses}>
+              全选
+            </span>
+          </Checkbox>
+        </FormItem>
+        <FormItem
+          style={{ float: "left" }}
+          required={false}>
           {getFieldDecorator(ATTACK_STAGE_DATA_INDEX, {
             initialValue: defaultValue[ATTACK_STAGE_DATA_INDEX],
           })(
             <Checkbox.Group>
               {
-                stageRowDataIndexes.map((i,index)=>(
+                stageRowDataIndexes.map((i, index) => (
                   <Checkbox key={`${index}-check`} value={i}>
-                  <span className={lblClasses}>
-                  {tableTextConfig.rowTitles[i]}
-                  </span>
+                    <span className={lblClasses}>
+                      {tableTextConfig.rowTitles[i]}
+                    </span>
                   </Checkbox>
                 ))
               }
             </Checkbox.Group>
-          )
+            )
           }
         </FormItem>
         <Button
