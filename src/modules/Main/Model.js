929a4b3c5f14f58cf6f17f8d1ad821d9430b0eca
chenxinzhou <chenxinzhou@antiy.cn>
2017-11-30 17:42:16 +0800

    modify/add debug mode shortcut key

--- src/modules/Main/Model.js
+++ src/modules/Main/Model.js
@@ -11,6 +11,9 @@ import {
   EVENT_TYPE_DATA_INDEX,
   NAMESPACE
 } from './ConstConfig.js'
+import Mousetrap from 'mousetrap'
+import { DEBUG_MODE } from 'configs/ConstConfig'
+import { Modal, message as Message } from 'antd'
 moment.locale('zh-cn');
 
 
@@ -22,6 +25,26 @@ const baseModel = {
     }
   },
   effects: {
+  },
+  subscriptions: {
+    setup: ({ history, dispatch }) => {
+      Mousetrap.bind(['command+alt+d+b', 'ctrl+alt+d+b'], function () {
+        Modal.confirm({
+          title: "DEBUG模式",
+          content: "是否开启DEBUG模式，将关闭数据的加密和解密模块（关闭tab后重启可取消DEBUG模式）",
+          okText: "确定",
+          onOk: () => {
+            sessionStorage.setItem(DEBUG_MODE, DEBUG_MODE)
+            if (sessionStorage.getItem(DEBUG_MODE)) {
+              Message.success("DEBUG模式开始成功")
+            } else {
+              Message.error("DEBUG模式开启失败")
+            }
+          }
+        })
+        return false;
+      });
+    }
   }
 };
 
