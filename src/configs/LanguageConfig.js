612b0db8ef57df626f593009b570fc1f61376860
chenxinzhou <chenxinzhou@antiy.cn>
2018-01-10 16:37:18 +0800

    modify/ids title

--- src/configs/LanguageConfig.js
+++ src/configs/LanguageConfig.js
@@ -175,7 +175,7 @@ export default {
     routes: routesENUS,
   },
   "zh-cn": {
-    title: (producType === IDS || producType === IDS_STAND_ALONE) ? "安天捕风蜜罐流量监测系统" : "安天捕风蜜罐系统",
+    title: (producType === IDS || producType === IDS_STAND_ALONE) ? "安天捕风流量监测系统" : "安天捕风蜜罐系统",
     routes: routesZHCN,
 
   }
