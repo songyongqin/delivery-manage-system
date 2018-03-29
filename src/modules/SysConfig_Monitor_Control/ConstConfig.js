cc121b1a7e3e0a81eda47f204a60d6791f6fcd3d
chenxinzhou <chenxinzhou@antiy.cn>
2018-03-29 15:22:56 +0800

    refactor/delete needless sysconfig monitor code

--- src/modules/SysConfig_Monitor_Control/ConstConfig.js
+++
@@ -1,6 +0,0 @@
-export const NAMESPACE = "monitorControl"
-export const MODULE_MONITOR_DATA_INDEX = "moduleMonitor"
-export { NAMESPACE as MAIN_NAMESPACE } from '../Main/ConstConfig'
-export { MODULE_LIST_DATA_INDEX, MONITOR_PERIOD_DATA_INDEX } from '../SysConfig_Monitor/ConstConfig'
-
-export { ERROR_VALUE, REGULAR_VALUE } from '../SysConfig_Monitor/ConstConfig'
