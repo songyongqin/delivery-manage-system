bc541f24862cdc03199bb7cf389e33da69e509c8
chenxinzhou <chenxinzhou@antiy.cn>
2018-04-23 16:55:36 +0800

    feature/handle signin overdue

--- src/domainUtils/request/final.ts
+++ src/domainUtils/request/final.ts
@@ -2,6 +2,9 @@ import isSuccess from 'domainUtils/isSuccess'
 import { message as Message } from 'antd'
 import ApiConfig from 'services/apiConfig'
 const httpApi = ApiConfig.http
+import { routerRedux } from 'dva/router'
+import { Modal } from 'antd'
+import { delUserData } from 'domain/user'
 
 Message.config({
   duration: 5,
@@ -12,12 +15,34 @@ const isIgnoreApi = (url: string, options: object) => {
   return [httpApi.CREATE_STATUS].includes(url)
 }
 
+const handleSignInOverdue = () => {
+  Modal.error({
+    title: "登录超时",
+    content: "登录超时,请重新登录",
+    okText: "确定",
+    onOk: () => {
+      delUserData()
+      window.location.reload()
+    }
+  })
+}
+
+const handleLicenceOverdue = () => {
+
+}
+
 export default (url: string, options: object, result: any): void => {
 
   try {
     url = url.split("?")[0]
     if (isSuccess(result) || isIgnoreApi(url, options)) {
       return
+    }
+    if (result.status === -4) {
+      return handleSignInOverdue()
+    }
+    if (result.status === -10) {
+      return handleLicenceOverdue()
     }
     Message.error(result.message || result.payload)
   } catch (e) {
