2fc9182fd97e709db13cf923fd3d8ea1bd7dcd0d
chenxinzhou <chenxinzhou@antiy.cn>
2018-04-25 16:29:58 +0800

    refactor/overall

--- src/modules/Analyse_Overall/components/CommonItem/index.tsx
+++ src/modules/Analyse_Overall/components/CommonItem/index.tsx
@@ -1,17 +1,38 @@
 import * as React from 'react'
 import TableWithRemote from 'domainComponents/TableWithRemote'
+import LimitForm from './LimitForm'
 
 export default class CommonItem extends React.Component<any, any>{
   constructor(props) {
     super(props)
+    this.state = {
+      filters: { limit: 10 },
+      lastReqTime: 0
+    }
+  }
+  onSubmit = filters => {
+    this.setState({
+      filters,
+      lastReqTime: new Date().getTime(),
+    })
   }
   render() {
 
 
     return (
       <div>
+        <div style={{ overflow: "hidden" }}>
+          <div style={{ float: "left" }}>
+            {this.props.expandPanel}
+          </div>
+          <div style={{ float: "left" }}>
+            <LimitForm onSubmit={this.onSubmit}></LimitForm>
+          </div>
+
+        </div>
         <TableWithRemote
-          initialFilters={this.props.initialFilters}
+          key={`${this.state.lastReqTime}-table`}
+          initialFilters={{ ...this.props.initialFilters, ...this.state.filters }}
           getColumns={this.props.getColumns}
           remoteNamespace={this.props.remoteNamespace}>
         </TableWithRemote>
