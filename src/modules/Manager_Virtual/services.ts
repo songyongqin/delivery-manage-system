05949c27b66054603b9302b7ef1b6b4e38ec3e6d
chenxinzhou <chenxinzhou@antiy.cn>
2018-04-25 14:59:12 +0800

    fix/vm validate params lost

--- src/modules/Manager_Virtual/services.ts
+++ src/modules/Manager_Virtual/services.ts
@@ -13,6 +13,6 @@ export const deleteVM = commonRequestCreator.deleteWithQueryString(httpApi.VIRTU
 
 export const fetchVMOption = commonRequestCreator.get(httpApi.VM_OPTION)
 
-export const validate = commonRequestCreator.get(httpApi.VALIDATE)
+export const validate = commonRequestCreator.getWithQueryString(httpApi.VALIDATE)
 
 export const fetchVMCreateStatus = commonRequestCreator.getWithQueryString(httpApi.CREATE_STATUS)
