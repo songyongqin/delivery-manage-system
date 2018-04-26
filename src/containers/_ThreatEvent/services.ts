f918dd602027e6543dcca1b67de2132a1147dec6
chenxinzhou <chenxinzhou@antiy.cn>
2018-04-26 09:38:07 +0800

    style/remove needless files

--- src/containers/_ThreatEvent/services.ts
+++
@@ -1,9 +0,0 @@
-import commonRequestCreator from 'domainUtils/commonRequestCreator'
-import ApiConfig from 'services/apiConfig'
-const httpApi = ApiConfig.http
-
-export const fetchThreatEventExploit = commonRequestCreator.getWithQueryString(httpApi.ANALYSE_EVENT_EXPLOIT)
-
-export const fetchThreatEventTool = commonRequestCreator.getWithQueryString(httpApi.ANALYSE_EVENT_TOOL)
-
-export const fetchTreatEventInfo = commonRequestCreator.getWithQueryString(httpApi.ANALYSE_EVENT_THREAT_INFO)
