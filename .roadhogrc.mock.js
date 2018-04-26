faa1d1377fb649eea7e0b5df84976365d9086a05
chenxinzhou <chenxinzhou@antiy.cn>
2018-04-26 13:13:08 +0800

    build/assign dva version

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,15 +1,7 @@
 
-import appConfig from './config/app.json'
-import analyseEventConfig from './config/table/analyseEvent.json'
-import threatEventExploit from './config/table/threatEventExploit.json'
-import threatEventThreatInfo from './config/table/threatEventThreatInfo.json'
-import threatEventTool from './config/table/threatEventTool.json'
+
 
 export default {
-  // '/static/config/appConfig.json': appConfig,
-  // '/static/config/table/analyseEvent.json': analyseEventConfig,
-  // '/static/config/table/threatEventExploit.json': threatEventExploit,
-  // '/static/config/table/threatEventThreatInfo.json': threatEventThreatInfo,
-  // '/static/config/table/threatEventTool.json': threatEventTool,
+
 }
 
