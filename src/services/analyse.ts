41831ad1774ef15d405389c158f694bf54539cb2
chenxinzhou <chenxinzhou@antiy.cn>
2018-04-28 19:51:41 +0800

    style/remove needless files

--- src/services/analyse.ts
+++
@@ -1,5 +0,0 @@
-import commonRequestCreator from 'domainUtils/commonRequestCreator'
-import ApiConfig from 'services/apiConfig'
-const httpApi = ApiConfig.http
-
-export const fetchAnalyseEvent = commonRequestCreator.getWithQueryString(httpApi.ANALYSE_EVENT)
