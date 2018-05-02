fd0d520cef7b302318fa76a45e33b12a444a9c52
chenxinzhou <chenxinzhou@antiy.cn>
2018-05-02 09:37:24 +0800

    fix/time label error

--- src/components/TimeLabel/index.tsx
+++ src/components/TimeLabel/index.tsx
@@ -3,5 +3,16 @@ import Tag from 'components/Tag'
 import moment from 'moment'
 
 export default ({ value }) => {
-  return <Tag color="#1890ff">{moment(value * 1000).format("YYYY-MM-DD HH:mm:ss")}</Tag>
+  try {
+    if (value.length === 1) {
+      return <Tag color="#1890ff">{moment(value * 1000).format("YYYY-MM-DD HH:mm:ss")}</Tag>
+    }
+    return [
+      <Tag color="#1890ff" key="nth-1">{moment(value[0] * 1000).format("YYYY-MM-DD HH:mm:ss")}</Tag>,
+      <span key="txt">至&nbsp;</span>,
+      <Tag color="#1890ff" key="nth-2">{moment(value[1] * 1000).format("YYYY-MM-DD HH:mm:ss")}</Tag>,
+    ]
+  } catch (e) {
+    return <Tag color="#1890ff">{moment(value * 1000).format("YYYY-MM-DD HH:mm:ss")}</Tag>
+  }
 }
