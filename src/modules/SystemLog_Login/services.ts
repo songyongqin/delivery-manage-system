7f8921f2ba1fe9a842ddd6830ff8abc501d4c7a7
chenxinzhou <chenxinzhou@antiy.cn>
2018-05-02 17:34:28 +0800

    fix/login log export ,timestampRange formatter error

--- src/modules/SystemLog_Login/services.ts
+++ src/modules/SystemLog_Login/services.ts
@@ -2,7 +2,18 @@ import commonRequestCreator from 'domainUtils/commonRequestCreator'
 import ApiConfig from 'services/apiConfig'
 const httpApi = ApiConfig.http
 import isSuccess from 'domainUtils/isSuccess'
+import momentToTimeStampRange from 'domainUtils/momentToTimeStampRange'
 
 export const fetchSystemLogLogin = commonRequestCreator.getWithQueryString(httpApi.SYS_LOG_LOGIN)
 
-export const exportSystemLogLogin = commonRequestCreator.post(httpApi.SYS_LOG_LOGIN)
+const _exportSystemLogLogin = commonRequestCreator.post(httpApi.SYS_LOG_LOGIN)
+
+export const exportSystemLogLogin = payload => {
+  if ("timestampRange" in payload) {
+    payload = {
+      ...payload,
+      timestampRange: momentToTimeStampRange(payload["timestampRange"])
+    }
+  }
+  return _exportSystemLogLogin(payload)
+}
