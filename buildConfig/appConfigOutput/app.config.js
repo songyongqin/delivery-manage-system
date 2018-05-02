42408d3d02d7c5a90aad002261b2582aef6748f6
chenxinzhou <chenxinzhou@antiy.cn>
2018-05-02 14:28:06 +0800

    build/add config for ids-standalone

--- buildConfig/appConfigOutput/app.config.js
+++ buildConfig/appConfigOutput/app.config.js
@@ -190,6 +190,104 @@ module.exports = {
       "phoneNumber": "0755-26806561",
       "email": "antiy_shenzhen@antiy.cn"
     }
+  },
+  //IDS单机版
+  "idsStandalone": {
+    "title": "安天捕风流量监测系统",
+    "secret": true,
+    "routerRegister": {
+      "/overview": true,
+      "/analyse": true,
+      "/analyse/event": true,
+      "/analyse/attack-chain": false,
+      "/analyse/fall-host": true,
+      "/analyse/ranking": true,
+      "/analyse/threat-distribution": true,
+      "/analyse/overall": true,
+      "/early-warning": true,
+      "/early-warning/email": true,
+      "/report": true,
+      "/sys-config": true,
+      "/sys-config/network": true,
+      "/sys-config/monitor": true,
+      "/sys-config/strategy": true,
+      "/user-manager": true,
+      "/manager": true,
+      "/manager/device": true,
+      "/manager/virtual-machine": false,
+      "/manager/mirror": false,
+      "/sys-log": true,
+      "/sys-log/login": true,
+      "/snort": true,
+      "/file-restore": true
+    },
+    "adminOnly": [
+      "/sys-config",
+      "/sys-config/network",
+      "/sys-config/monitor",
+      "/sys-config/strategy",
+      "/sys-log",
+      "/sys-log/login",
+      "/user-manager",
+      "/snort"
+    ],
+    "routerTitleConfig": {
+      "/manager": "流量监测管理"
+    },
+    "recordOfCreatingVM": false,
+    "ipInfo": {
+      "idsNode": false,
+      "honeypotNode": false
+    },
+    "overview": {
+      "captureFlow": true,
+      "threatEvent": true
+    },
+    "analyse": {},
+    "overall": {
+      "capture": false,
+      "netBasic": true,
+      "limitNetBasic": true,
+      "pcap": false,
+      "system": false
+    },
+    "networkConfig": {
+      "dns": true,
+      "network": true,
+      "systemLog": true,
+      "networkAuth": true,
+      "masterIPConfig": true,
+      "cloudDetection": true
+    },
+    "strategyConfig": {
+      "strategy": true,
+      "white": false
+    },
+    "monitor": {
+      "master": false,
+      "honeypotStandalone": false,
+      "honeypot": false,
+      "ids": true
+    },
+    "mainDevice": "master",
+    "deviceManager": {
+      "master": false,
+      "honeypot": false,
+      "ids": false,
+      "honeypotNode": false,
+      "idsNode": false,
+      "honeypotStandalone": false,
+      "idsStandalone": true
+    },
+    "mirrorImageManager": {
+      "collection": true,
+      "node": true,
+      "nodeReadOnly": false
+    },
+    "contactInfo": {
+      "phoneNumber": "0755-26806561",
+      "email": "antiy_shenzhen@antiy.cn"
+    }
   }
 }
 
