1e5990d4ce527dba55cf3857876f7ab9f536b392
pengwei <pengwei@antiy.cn>
2019-07-19 11:15:45 +0800

    change analyse threat intelligence api pre

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -264,15 +264,15 @@ const HttpApiConfig = {
 
   CONFIG_TIME: '/config/time',
 
-  ANALYSE_THREAT_INTELLIGENCE_COUNT: '/analyse-threat-intelligence/count',
+  ANALYSE_THREAT_INTELLIGENCE_COUNT: '/analyse/threat-intelligence/count',
 
-  ANALYSE_THREAT_INTELLIGENCE_TABLE: '/analyse-threat-intelligence/table',
+  ANALYSE_THREAT_INTELLIGENCE_TABLE: '/analyse/threat-intelligence/table',
 
-  ANALYSE_THREAT_INTELLINGENCE_UPLOAD: '/analyse-threat-intelligence/upload',
+  ANALYSE_THREAT_INTELLINGENCE_UPLOAD: '/analyse/threat-intelligence/upload',
 
   // ANALYSE_THREAT_INTELLINGENCE_UPLOAD: '/mock/uploads', //模拟
 
-  ANALYSE_THREAT_INTELLIGENCE_INFO: '/analyse-threat-intelligence/info',
+  ANALYSE_THREAT_INTELLIGENCE_INFO: '/analyse/threat-intelligence/info',
 
 }
 
